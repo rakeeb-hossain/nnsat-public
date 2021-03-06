e50330a8054619559fbe0b31bdbb9e81582894aa
rakeeb123 <rakeeb.hossain1@gmail.com>
2021-03-06 18:25:06 -0500

    Benchmarks added

---
+++ benchmarks/generate.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# 1. SATCOMP 2020 Main benchmarks
+if false; then
+echo "=============================GENERATING SATCOMP2020============================"
+mkdir -p sc2020
+cd sc2020
+
+uri_file="../sc2020-main.uri"
+
+cat $uri_file | while read URL
+do
+	FILE=${URL##*/}
+	CNF_FILE=${FILE::-3}
+	if [ ! -f "$CNF_FILE" ]; then
+		wget -q --show-progress $URL
+		unxz $FILE
+	fi
+done
+
+cd ..
+fi
+
+# 2. Crypto benchmarks 
+if false; then
+echo "=============================GENERATING CRYPTO==============================="
+./make_encoder.sh
+
+mkdir -p crypto
+cd crypto
+
+ROUNDS=20
+NUM_INSTANCES=50
+CTR=1
+
+while [ $CTR -le $NUM_INSTANCES ]
+do
+	./../crypto-encoding --function sha1 --rounds $ROUNDS --target random > "sha256-$ROUNDS-preimage$CTR.cnf" 2>/dev/null
+	echo "Instance $CTR done"
+	CTR=$((CTR+1))
+done
+
+cd ..
+
+fi
+
+# 3. Hard combinatorial benchmarks
+if true; then
+echo "===========================GENERATING COMBINATORIAL============================="
+
+wget -q --show-progress "https://baldur.iti.kit.edu/sat-competition-2016/downloads/crafted16.zip"
+unzip "crafted16.zip"
+
+# rm "crafted16.zip"
+
+fi
---
+++ benchmarks/make_encoder.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd SAT-encoding/crypto && make 
+
+mv main ../../crypto-encoding
